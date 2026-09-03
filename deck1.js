846d320b9049d91720b13801164d644eaeeb4a13
Kato819 <116700163+Kato819@users.noreply.github.com>
2026-09-03 16:13:56 +0900

    Update deck1.js

--- deck1.js
+++ deck1.js
@@ -11,7 +11,14 @@ const deck1Recipe = [
     { cardId: "bs-10", count: 3 },
     { cardId: "bs-11", count: 1 },
     { cardId: "bs-12", count: 2 },
-    { cardId: "bs-13", count: 2 }
+    { cardId: "bs-13", count: 2 },
+    { cardId: "bs-14", count: 2 },
+    { cardId: "bs-15", count: 3 },
+    { cardId: "bs-16", count: 2 },
+    { cardId: "bs-17", count: 1 },
+    { cardId: "bs-18", count: 2 },
+    { cardId: "bs-19", count: 1 },
+    { cardId: "bs-20", count: 2 }
 
 ];
 
