9017b513b4ce2b57da685d241a550b6aa996151b
Kato819 <116700163+Kato819@users.noreply.github.com>
2026-09-03 11:47:54 +0900

    Add files via upload

---
+++ card-list.js
@@ -0,0 +1,81 @@
+// --- カードマスターデータ（図鑑データ） ---
+const CARD_DATABASE = {
+    "bs-01": {
+        name: "Sagitt-Apollodragon",
+        type: "spirit", // spirit, brave, nexus, magic 等
+        cost: 8,
+        image: "images/sagitt_apollodragon.png"
+    },
+    "bs-02": {
+        name: "Blade-Ra",
+        type: "spirit",
+        cost: 0,
+        image: "images/blade_ra.jpg"
+    },
+    "bs-03": {
+        name: "Darkwurm-Nova",
+        type: "spirit",
+        cost: 8,
+        image: "images/darkwurm_nova.jpg"
+    },
+    "bs-04": {
+        name: "Veloci-Harper",
+        type: "spirit",
+        cost: 8,
+        image: "images/veloci_harper.png"
+    },
+    "bs-05": {
+        name: "The SunDragon Apollodragon",
+        type: "spirit",
+        cost: 8,
+        image: "images/apollodragon.png"
+    },
+    "bs-06": {
+        name: "The StarEmperorDragon Geminize",
+        type: "spirit",
+        cost: 8,
+        image: "images/geminize.png"
+    },
+    "bs-07": {
+        name: "Iguabaggy",
+        type: "spirit",
+        cost: 8,
+        image: "images/iguabaggy.png"
+    },
+    "bs-08": {
+        name: "Starry Draw",
+        type: "spirit",
+        cost: 8,
+        image: "images/starry_draw.png"
+    },
+    "bs-09": {
+        name: "The DragonBullEmperor Dragonic-Taurus",
+        type: "spirit",
+        cost: 8,
+        image: "images/dragonic_taurus.png"
+    },
+    "bs-10": {
+        name: "Morgasaurus",
+        type: "spirit",
+        cost: 8,
+        image: "images/morgasaurus.png"
+    },
+    "bs-11": {
+        name: "The SunDragonLord Rising-Apollodragon",
+        type: "spirit",
+        cost: 8,
+        image: "images/rising_apollodragon.png"
+    },
+    "bs-12": {
+        name: "Revive Draw",
+        type: "spirit",
+        cost: 8,
+        image: "images/revive_draw.png"
+    },
+    "bs-13": {
+        name: "Sagitta Flame",
+        type: "spirit",
+        cost: 8,
+        image: "images/sagitta_flame.png"
+    },
+};
