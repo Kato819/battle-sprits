1203c20f717201140d885d0f30c187d1c549c869
Kato819 <116700163+Kato819@users.noreply.github.com>
2026-09-04 19:00:45 +0900

    Update card-list.js

--- card-list.js
+++ card-list.js
@@ -119,5 +119,137 @@ const CARD_DATABASE = {
         type: "spirit",
         cost: 8,
         image: "images/tres_beluga.png"
+    },
+    "bs-21": {
+        name: "storm_draw",
+        type: "spirit",
+        cost: 8,
+        image: "images/storm_draw.png"
+    },
+    "bs-22": {
+        name: "ralba",
+        type: "spirit",
+        cost: 8,
+        image: "images/ralba.png"
+    },
+    "bs-23": {
+        name: "Northernbeard",
+        type: "spirit",
+        cost: 8,
+        image: "images/Northernbeard.png"
+    },
+    "bs-24": {
+        name: "Pendragon",
+        type: "spirit",
+        cost: 8,
+        image: "images/Pendragon.png"
+    },
+    "bs-25": {
+        name: "life_charge",
+        type: "spirit",
+        cost: 8,
+        image: "images/life_charge.png"
+    },
+    "bs-26": {
+        name: "Protect_Aura",
+        type: "spirit",
+        cost: 8,
+        image: "images/Protect_Aura.png"
+    },
+    "bs-27": {
+        name: "Cancerd",
+        type: "spirit",
+        cost: 8,
+        image: "images/Cancerd.png"
+    },
+    "bs-28": {
+        name: "Strike-Siegwurm",
+        type: "spirit",
+        cost: 8,
+        image: "images/Strike-Siegwurm.png"
+    },
+    "bs-29": {
+        name: "Scor-Spear",
+        type: "spirit",
+        cost: 8,
+        image: "images/Scor-Spear.png"
+    },
+    "bs-30": {
+        name: "Danderabbit",
+        type: "spirit",
+        cost: 8,
+        image: "images/Danderabbit.png"
+    },
+    "bs-31": {
+        name: "Dark_Sacred_Sword",
+        type: "spirit",
+        cost: 8,
+        image: "images/Dark_Sacred_Sword.png"
+    },
+    "bs-32": {
+        name: "Reboot_Code",
+        type: "spirit",
+        cost: 8,
+        image: "images/Reboot_Code.png"
+    },
+    "bs-33": {
+        name: "Aqua-Elysion",
+        type: "spirit",
+        cost: 8,
+        image: "images/Aqua-Elysion.png"
+    },
+    "bs-34": {
+        name: "Libra-Golem",
+        type: "spirit",
+        cost: 8,
+        image: "images/Libra-Golem.png"
+    },
+    "bs-35": {
+        name: "Swordoll",
+        type: "spirit",
+        cost: 8,
+        image: "images/Swordoll.png"
+    },
+    "bs-36": {
+        name: "Manekicat",
+        type: "spirit",
+        cost: 8,
+        image: "images/Manekicat.png"
+    },
+    "bs-37": {
+        name: "Light_Sacred_Sword",
+        type: "spirit",
+        cost: 8,
+        image: "images/Light_Sacred_Sword.png"
+    },
+    "bs-38": {
+        name: "Stein-Bolg",
+        type: "spirit",
+        cost: 8,
+        image: "images/Stein-Bolg.png"
+    },
+    "bs-39": {
+        name: "Lunatech-Strikewurm",
+        type: "spirit",
+        cost: 8,
+        image: "images/Lunatech-Strikewurm.png"
+    },
+    "bs-40": {
+        name: "Hawk-Breaker",
+        type: "spirit",
+        cost: 8,
+        image: "images/Hawk-Breaker.png"
+    },
+    "bs-41": {
+        name: "Asklepiooze",
+        type: "spirit",
+        cost: 8,
+        image: "images/Asklepiooze.png"
+    },
+    "bs-42": {
+        name: "Strikewurm-Leo",
+        type: "spirit",
+        cost: 8,
+        image: "images/Strikewurm-Leo.png"
     }
 };
